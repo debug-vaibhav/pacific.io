b1b9e61b41bb3fb2b2cf83abd852bc17ed3891cc
Vaibhav KB <vaibhav.bachhav@ideas.com>
2023-01-30 13:29:43 +0530

    feat: shifted to react-scripts

--- client/src/pages/configuration/Configuration.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Configuration = () => {
-    return <div>Configuration</div>;
-};
-
-export default Configuration;
--- client/src/services/instances/common.instance.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const commonInstance = axios.create({
-    baseURL: `http://${process.env.API_SERVER_URL}/api/v1`,
-    timeout: 5000,
-    responseType: 'json',
-    responseEncoding: 'utf8',
-});
-export default commonInstance;
